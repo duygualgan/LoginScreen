7cd022faac84afcb72d4df89a35c7d47e06ac56f
Duygu Algan <duygu.algan.esk26@gmail.com>
2022-07-02 18:51:33 +0300

    Uı design of login screen
    Uı design of login screen

---
+++ LoginPageTasarm/app/src/main/java/com/example/loginpagetasarm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.loginpagetasarm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
